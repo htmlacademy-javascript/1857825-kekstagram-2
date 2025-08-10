548e4be735dade75b49621ed67b559318d5e4689
Kravtsova Tatyana <tanayakravtsova@gmail.com>
2025-08-10 11:53:23 +0300

    functions return

--- js/functions.js
+++ js/functions.js
@@ -24,3 +24,24 @@ const getCeilNumber = (data) => {
 };
 getCeilNumber('54iop68');
 /*console.log(getCeilNumber(-7));*/
+
+function checkDurationMeeting (startWork, endWork, startMeeting, durationMeeting) {
+  const toMinutes = (timeStr) => {
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours * 60 + minutes;
+  };
+
+  const workStart = toMinutes(startWork);
+  const workEnd = toMinutes(endWork);
+  const meetingStart = toMinutes(startMeeting);
+  const meetingEnd = meetingStart + durationMeeting;
+
+  const workDuration = workEnd - workStart;
+  return meetingStart >= workStart && meetingEnd <= workEnd && durationMeeting <= workDuration;
+}
+checkDurationMeeting('08:00', '17:30', '14:00', 90); // true
+checkDurationMeeting('8:0', '10:0', '8:0', 120);// true
+checkDurationMeeting('08:00', '14:30', '14:00', 90); // false
+checkDurationMeeting('14:00', '17:30', '08:0', 90);// false
+checkDurationMeeting('8:00', '17:30', '08:00', 900);// false
+
