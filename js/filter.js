00fd726f96041673946f8c5fd7002886252e2a7e
Kravtsova Tatyana <tanayakravtsova@gmail.com>
2025-09-16 13:05:08 +0300

    add filter and debounce

---
+++ js/filter.js
@@ -0,0 +1,51 @@
+import { debounce } from './utils.js';
+import { clearGallary } from './render.js';
+import { removeClass, addClass } from './domUtils.js';
+
+// Константы для классов
+const ACTIVE_BUTTON_CLASS = 'img-filters__button--active';
+const BUTTON_SELECTOR = '.img-filters__button';
+
+const Filter = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed'
+};
+
+const getFilteredPhotos = (photos, filterType) => {
+  switch (filterType) {
+    case Filter.RANDOM:
+      return [...photos].sort(() => Math.random() - 0.5).slice(0, 10);
+    case Filter.DISCUSSED:
+      return [...photos].sort((a, b) => b.comments.length - a.comments.length);
+    case Filter.DEFAULT:
+    default:
+      return [...photos];
+  }
+};
+
+const initFilters = (photos, renderGalleryCallback, container) => {
+  const filtersContainer = document.querySelector('.img-filters');
+  removeClass(filtersContainer, 'img-filters--inactive');
+
+  const onFilterClick = debounce((evt) => {
+    const filterButton = evt.target.closest(BUTTON_SELECTOR);
+
+    if (!filterButton || filterButton.classList.contains(ACTIVE_BUTTON_CLASS)) {
+      return;
+    }
+
+    // Более читаемый и производительный вариант
+    const activeButtons = document.querySelectorAll(`${BUTTON_SELECTOR}.${ACTIVE_BUTTON_CLASS}`);
+    activeButtons.forEach((button) => removeClass(button, ACTIVE_BUTTON_CLASS));
+    addClass(filterButton, ACTIVE_BUTTON_CLASS);
+    clearGallary(container);
+
+    const filteredPhotos = getFilteredPhotos(photos, filterButton.id);
+    renderGalleryCallback(filteredPhotos, container);
+  }, 500);
+
+  filtersContainer.addEventListener('click', onFilterClick);
+};
+
+export { initFilters };
--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-import './render.js';
-import './renderBigPicture.js';
+import { getData } from './api.js';
+import { renderGallery, showLoadError } from './render.js';
+import { initBigPicture } from './renderBigPicture.js';
+import { initFilters } from './filter.js';
 import './formUpload.js';
 import './editLoadImg.js';
 import './noUiSlider.js';
+
+const usersPhotoList = document.querySelector('.pictures');
+let similarPhotoDescriptions = [];
+
+const rerenderGallery = (photos, container) => {
+  renderGallery(photos, container);
+};
+
+const initApp = async () => {
+  try {
+    similarPhotoDescriptions = await getData();
+    // Первоначальная отрисовка
+    renderGallery(similarPhotoDescriptions, usersPhotoList);
+    // Инициализируем функционал большого фото
+    initBigPicture(similarPhotoDescriptions, usersPhotoList);
+    // Инициализируем фильтры, передавая:
+    // 1. Данные
+    // 2. Функцию для перерисовки
+    // 3. Контейнер
+    initFilters(similarPhotoDescriptions, rerenderGallery, usersPhotoList);
+  } catch (error) {
+    similarPhotoDescriptions = [];
+    showLoadError();
+  }
+};
+
+initApp();
